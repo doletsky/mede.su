8c577ec9b97d03cb49fe1633f762ff468ec4f2aa
adoletskiy <adoletsky@indigos.ru>
2017-04-06 15:28:51 +0300

    debug top menu

--- bitrix/templates/mede/components/bitrix/menu/top_mede/template.php
+++ bitrix/templates/mede/components/bitrix/menu/top_mede/template.php
@@ -1,11 +1,10 @@
 <?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
-
     <ul class="menu">
-        <li><a class="curr" href="/">Главная</a></li>
+        <li><a <?if($APPLICATION->GetCurPage()=='/'):?>class="curr" <?endif?>href="/">Главная</a></li>
 <?foreach($arResult as $arItem):?>
         <li>
-            <a class="" href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a>
+            <a <?if($APPLICATION->GetCurPageParam()==$arItem["LINK"]):?>class="curr" <?endif?>href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a>
         </li>
 <?endforeach?>
-        <li><a class="curr" href="/search.php">Найти</a></li>
+        <li><a <?if($APPLICATION->GetCurPage()=='/search.php'):?>class="curr" <?endif?>href="/search.php">Найти</a></li>
     </ul>
