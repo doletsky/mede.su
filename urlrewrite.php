6c8a161e8f1b2a05e6450f058108899c15ceca4a
adoletskiy <adoletsky@indigos.ru>
2017-07-31 12:20:40 +0300

    debug error search links

--- urlrewrite.php
+++ urlrewrite.php
@@ -1,13 +1,13 @@
 <?
 $arUrlRewrite = array(
 	array(
-		"CONDITION" => "#^/(.*)/(.*)/#",
+		"CONDITION" => "#^/(.*)/(.*)/(.*)#",
 		"RULE" => "CODE=$2",
 		"ID" => "bitrix:news.detail",
 		"PATH" => "/detail.php",
 	),
     array(
-        "CONDITION" => "#^/(.*)/#",
+        "CONDITION" => "#^/(.*)/(.*)#",
         "RULE" => "CODE=$1",
         "ID" => "bitrix:news.list",
         "PATH" => "/list.php",
